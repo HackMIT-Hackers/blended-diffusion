2a0c92669df256981ac178eb7351faeb2a716786
anirudh.rahul <anirudhrahul@anjurahul.com>
2022-09-17 10:55:18 -0400

    Adding image editor to serve

--- serve.py
+++ serve.py
@@ -1,5 +1,6 @@
-# from optimization.image_editor import ImageEditor
+from optimization.image_editor import ImageEditor
 # from optimization.arguments import get_arguments
+import argparse
 from flask import Flask, render_template, request
 from functools import wraps
 import base64
@@ -40,8 +41,19 @@ def process(body):
 
     with open("temp.jpg", "wb") as fh:
         fh.write(base64.b64decode(imageData))
-
     key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+
+    async def runAlgo():
+        args = argparse.Namespace(prompt=body["prompt"], 
+        init_image='input_example/temp.jpg', mask='input_example/tempMask.png', 
+        skip_timesteps=25, local_clip_guided_diffusion=False, ddim=False, timestep_respacing='100', 
+        model_output_size=256, aug_num=8, clip_guidance_lambda=1000, range_lambda=50, lpips_sim_lambda=1000, l2_sim_lambda=10000, 
+        background_preservation_loss=False, invert_mask=False, enforce_background=True, seed=404, gpu_id=0, 
+        output_path='output', output_file=f'{key}.png', iterations_num=1, batch_size=2, save_video=False, export_assets=False)
+        image_editor = ImageEditor(args)
+        image_editor.edit_image_by_prompt()
+        image_editor.reconstruct_image()
+        
     tasks[key] = imageData
     url =  f"http://localhost:9000/pollTask?key={key}"
     return url, 200
