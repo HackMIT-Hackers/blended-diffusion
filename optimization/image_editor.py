48908b0a7cf5372e5a2257b6a5ebc2e3533f5b5a
anirudh.rahul <anirudhrahul@anjurahul.com>
2022-09-18 01:46:12 -0400

    Update image_editor.py

--- optimization/image_editor.py
+++ optimization/image_editor.py
@@ -347,7 +347,7 @@ class ImageEditor:
                                 best_path = ranked_pred_path
                             outImg = pred_image_pil.resize(originalDimensions, Image.LANCZOS)
                             if cropDims:
-                                originalDimensions = (cropDims[2]-cropDims[0], cropDims[3]-cropsDims[1])
+                                originalDimensions = (cropDims[2]-cropDims[0], cropDims[3]-cropDims[1])
                                 out = pred_image_pil.resize(originalDimensions, Image.LANCZOS)
                                 init_image_pil = Image.open(self.args.init_image).convert("RGB")
                                 init_image_pil.paste(cropDims[0], cropDims[1])
